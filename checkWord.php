86ea68a18905b6b8d7c0714374cb44896c936449
rc2pc2 <petriccarcc@gmail.com>
2023-07-03 12:00:50 +0200

    example usage of get within the same php file

---
+++ checkWord.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Check word</title>
+</head>
+<body>
+
+    <?php
+
+        $words = [
+            'mare',
+            'sole',
+            'vesuvio',
+            'pizza',
+            'mozzarella',
+            'paperino'
+        ];
+
+        if ( empty($_GET['word'])){
+            echo 'Please type a word to see if it is present';
+        } else {
+            // c'e' nell'array?
+            if ( in_array($_GET['word'], $words) ){
+                echo 'found it!';
+            } else {
+                echo '4000004 not found';
+            }
+
+            // non c'e' nell'array
+
+        }
+
+    ?>
+
+    <form action="./checkWord.php" method="get">
+
+        <label for="word">
+            Check if a word is present
+        </label>
+        <input type="text" name="word" id="word">
+
+        <input type="submit" value="check">
+    </form>
+
+</body>
+</html>
+
