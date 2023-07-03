24604a1a8f999d6e0eee5c631ceaf934f236c4b7
rc2pc2 <petriccarcc@gmail.com>
2023-07-03 11:46:09 +0200

    li style added

--- secondIndex.php
+++ secondIndex.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fundamentals 1.2</title>
+    <link rel="stylesheet" href="./styles/style.css">
 </head>
 <body>
     <h1>
@@ -44,7 +45,7 @@
 
     <ul>
         <?php for ($i=0; $i < 10 ; $i++) { ?>
-            <li>
+            <li class="custom-list">
                 <p>
                     Element number <?php echo $i; ?>
                 </p>
