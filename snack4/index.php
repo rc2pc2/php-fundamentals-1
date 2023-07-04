c0125fb9e9c98d87c08c0b203c0f939ca4591e3f
rc2pc2 <petriccarcc@gmail.com>
2023-07-04 13:06:15 +0200

    removed useless comments

--- snack4/index.php
+++ snack4/index.php
@@ -1,13 +1,4 @@
-
-
-Scrivi una funzione che accetti tre argomenti:
-un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo quanto il numero di elementi dell’array).
-La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b”
-
-
 <?php
-
-
 function customArraySlice( $array, $minIndex, $maxIndex){
 
         if ((count($array) < $maxIndex ) || ($minIndex > $maxIndex)
